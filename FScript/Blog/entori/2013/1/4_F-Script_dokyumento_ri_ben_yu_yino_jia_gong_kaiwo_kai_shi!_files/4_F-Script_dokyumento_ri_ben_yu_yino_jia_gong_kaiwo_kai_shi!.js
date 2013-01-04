b6729c5504cdfd44bd10e853d3c973bf37a64f29
hid2718 <hid2.71828o.haga@gmail.com>
2013-01-05 01:56:00 +0900

    all commit

--- FScript/Blog/entori/2013/1/4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!_files/4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!.js
+++ FScript/Blog/entori/2013/1/4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!_files/4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!.js
@@ -1,11 +1,9 @@
 // Created by iWeb 3.0.4 local-build-20130105
 
-setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
-function hostedOnDM()
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {dynamicallyPopulate();loadMozillaCSS('4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!_files/4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!Moz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id4');BlogFixupPreviousNext();applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id4');BlogFixupPreviousNext();performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
