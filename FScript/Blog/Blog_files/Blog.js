c3a75d6fc714e33232b204fd716f7649308ec1c8
hid2718 <hid2.71828o.haga@gmail.com>
2013-01-04 16:01:42 +0900

    all commit

--- FScript/Blog/Blog_files/Blog.js
+++ FScript/Blog/Blog_files/Blog.js
@@ -5,7 +5,7 @@ setTransparentGifURL('../Media/transparent.gif');function applyEffects()
 function hostedOnDM()
 {return false;}
 function photocastSubscribe()
-{photocastHelper("http://hid2718.github.com/FScript/FScript/Blog/rss.xml");}
+{photocastHelper("http://hid2718.github.com/FScript/Blog/rss.xml");}
 function onPageLoad()
 {loadMozillaCSS('Blog_files/BlogMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');fixupAllIEPNGBGs();applyEffects()}
--- FScript/Documents_files/Documents.js
+++ FScript/Documents_files/Documents.js
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Documents_files/DocumentsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
