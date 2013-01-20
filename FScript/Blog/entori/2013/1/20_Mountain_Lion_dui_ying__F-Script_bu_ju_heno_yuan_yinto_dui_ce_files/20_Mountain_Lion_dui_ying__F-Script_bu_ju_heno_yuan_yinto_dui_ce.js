35ee676e5a83f65dde8a48c2b888978196c08a09
hid2718 <hid2.71828o.haga@gmail.com>
2013-01-21 00:49:33 +0900

    all commit

---
+++ FScript/Blog/entori/2013/1/20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20130121
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,313),url:'20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/stroke_1.png'},{rect:new IWRect(1,-1,406,2),url:'20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/stroke_2.png'},{rect:new IWRect(407,-1,2,2),url:'20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/stroke_3.png'},{rect:new IWRect(407,1,2,313),url:'20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/stroke_4.png'},{rect:new IWRect(407,314,2,2),url:'20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/stroke_5.png'},{rect:new IWRect(1,314,406,2),url:'20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/stroke_6.png'},{rect:new IWRect(-1,314,2,2),url:'20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/stroke_7.png'}],new IWSize(408,315))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ce_files/20_Mountain_Lion_dui_ying__F-Script_bu_ju_heno_yuan_yinto_dui_ceMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id5');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
--- FScript/Blog/entori/2013/1/4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!_files/4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!.js
+++ FScript/Blog/entori/2013/1/4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!_files/4_F-Script_dokyumento_ri_ben_yu_yino_jia_gong_kaiwo_kai_shi!.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20130113
+// Created by iWeb 3.0.4 local-build-20130121
 
 setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
