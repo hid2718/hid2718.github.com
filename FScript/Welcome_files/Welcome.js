8c345f62b494860c7a7bd844e349a92faa15e9ad
hid2718 <hid2.71828o.haga@gmail.com>
2013-05-27 00:59:49 +0900

    all commit

--- FScript/Welcome_files/Welcome.js
+++ FScript/Welcome_files/Welcome.js
@@ -1,7 +1,7 @@
 // Created by iWeb 3.0.4 local-build-20130527
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_1:new IWReflection({opacity:0.50,offset:1.00}),reflection_0:new IWReflection({opacity:0.50,offset:1.00}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),reflection_0:new IWReflection({opacity:0.50,offset:1.00}),reflection_1:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
