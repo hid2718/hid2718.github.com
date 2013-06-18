4bc9f176bbfa47ed2e0c94b39e7c432b552a051f
hid2718 <hid2.71828o.haga@gmail.com>
2013-06-19 00:08:59 +0900

    all commit

---
+++ FScript/Blog/entori/2013/1/23_[ri_zhi]Smalltalk-users-jp_ni_F-Script-ja_henorinku_files/23_[ri_zhi]Smalltalk-users-jp_ni_F-Script-ja_henorinku.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130619
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('23_[ri_zhi]Smalltalk-users-jp_ni_F-Script-ja_henorinku_files/23_[ri_zhi]Smalltalk-users-jp_ni_F-Script-ja_henorinkuMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();fixupIECSS3Opacity('id4');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ FScript/Blog/entori/2013/2/9_[ru_men]u~indouwo_yitsu_biao_shishitemiyou_files/9_[ru_men]u~indouwo_yitsu_biao_shishitemiyou.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130619
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('9_[ru_men]u~indouwo_yitsu_biao_shishitemiyou_files/9_[ru_men]u~indouwo_yitsu_biao_shishitemiyouMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id4');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
