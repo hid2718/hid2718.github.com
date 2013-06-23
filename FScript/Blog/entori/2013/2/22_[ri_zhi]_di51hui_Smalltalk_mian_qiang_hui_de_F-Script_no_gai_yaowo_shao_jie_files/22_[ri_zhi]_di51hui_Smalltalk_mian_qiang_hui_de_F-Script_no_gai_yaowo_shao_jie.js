13706ac1688ff4e12501c1de8e66aedd65ae34ae
hid2718 <hid2.71828o.haga@gmail.com>
2013-06-24 01:30:25 +0900

    all commit

--- FScript/Blog/entori/2013/2/22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie.js
+++ FScript/Blog/entori/2013/2/22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie.js
@@ -4,6 +4,6 @@ setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {dynamicallyPopulate();loadMozillaCSS('22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jieMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();fixupIECSS3Opacity('id4');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();fixupIECSS3Opacity('id4');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
