c65d0c18230cfaabc5821d0c61557d19035336d7
hid2718 <hid2.71828o.haga@gmail.com>
2013-06-24 00:15:17 +0900

    all commit

---
+++ FScript/Blog/entori/2013/2/22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20130624
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,313),url:'22_%5Bri_zhi%5D_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'22_%5Bri_zhi%5D_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/stroke_1.png'},{rect:new IWRect(1,-1,406,2),url:'22_%5Bri_zhi%5D_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/stroke_2.png'},{rect:new IWRect(407,-1,2,2),url:'22_%5Bri_zhi%5D_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/stroke_3.png'},{rect:new IWRect(407,1,2,313),url:'22_%5Bri_zhi%5D_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/stroke_4.png'},{rect:new IWRect(407,314,2,2),url:'22_%5Bri_zhi%5D_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/stroke_5.png'},{rect:new IWRect(1,314,406,2),url:'22_%5Bri_zhi%5D_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/stroke_6.png'},{rect:new IWRect(-1,314,2,2),url:'22_%5Bri_zhi%5D_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/stroke_7.png'}],new IWSize(408,315))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jie_files/22_[ri_zhi]_di51hui_Smalltalk_mian_qiang_hui_de_F-Script_no_gai_yaowo_shao_jieMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();fixupIECSS3Opacity('id5');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
--- FScript/Welcome_files/Welcome.js
+++ FScript/Welcome_files/Welcome.js
@@ -1,7 +1,7 @@
-// Created by iWeb 3.0.4 local-build-20130619
+// Created by iWeb 3.0.4 local-build-20130624
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00}),stroke_0:new IWEmptyStroke(),reflection_1:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_1:new IWReflection({opacity:0.50,offset:1.00}),stroke_0:new IWEmptyStroke(),reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
